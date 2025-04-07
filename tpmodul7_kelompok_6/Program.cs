b02e9e118c3d865390e02c88800086954a06e546
dinasalsabilla <dinasalsabilla05@gmail.com>
2025-04-07 20:03:56 +0700

    menambahkan class DataMahasiswa103022300153

--- tpmodul7_kelompok_6/Program.cs
+++ tpmodul7_kelompok_6/Program.cs
@@ -5,20 +5,23 @@
         static void Main(string[] args)
         {
             
-            //DataMahasiswa103022300014.ReadJSON();
-            Console.WriteLine();
-            //KuliahMahasiswa103022300014.ReadJSON();
+            ////DataMahasiswa103022300014.ReadJSON();
+            ////Console.WriteLine();
+            ////KuliahMahasiswa103022300014.ReadJSON();
 
-
-            Console.WriteLine();
-            DataMahasiswa103022300065.ReadJSON();
-            Console.WriteLine();
-            KuliahMahasiswa103022300065.ReadJSON();   
-            Console.WriteLine();
+            //Console.WriteLine();
+            //DataMahasiswa103022300065.ReadJSON();
+            //Console.WriteLine();
+            //KuliahMahasiswa103022300065.ReadJSON();   
+            //Console.WriteLine();
 
             //DataMahasiswa103022300072.ReadJSON();
-            Console.WriteLine(); 
+            //Console.WriteLine();
             //KuliahMahasiswa103022300072.ReadJSON();
+
+            //Console.WriteLine();
+            DataMahasiswa103022300153.ReadJSON();
+            //Console.WriteLine();
         }
     }
 }
